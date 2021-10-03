0bca5207a520fccf5f5f8692fb403a64a060dfcb
Olexiy Mel'nychuk <alekseymelnych132@gmail.com>
2021-10-03 23:40:35 +0300

    Add Homepage Header

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,34 @@
-// // Import vendor jQuery plugin example
-// import '~/app/libs/mmenu/dist/mmenu.js'
+import {Swiper, Pagination, Navigation, Scrollbar} from 'swiper';
 
+Swiper.use([Pagination, Navigation, Scrollbar]);
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	const homeHeaderSlider = new Swiper('.home-header__slider', {
+		loop: false,
+		slidesPerView: 1,
+		speed: 2000,
+		navigation: {
+			prevEl: '.header-slider__button-prev',
+			nextEl: '.header-slider__button-next',
+		},
+		scrollbar: {
+			el: '.header-slider__scrollbar',
+			draggable: true
+		},
+		pagination: {
+			el: '.header-slider__pagination',
+			clickable: false,
+			type: 'custom',
+			renderCustom: function (swiper, current, total) { 
+				function addZero(number) {
+					return (number > 9) ? number : `0${number}`;
+				}
+				let currentNum = addZero(current);
+				let totalNum = addZero(total);
+				return `<span class='pagination-current'>${currentNum}</span><span class='pagination-total'>${totalNum}</span>`;
+			 },
+		}
+
+	})
 
 })
